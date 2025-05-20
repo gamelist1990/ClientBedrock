c124b032c2400bcd454e2db2a9d08c417e66dd4e
Koukunn_ <testbing@outlook.jp>
2025-05-21 00:10:37 +0900

    Update

--- other/discord/src/modules/Other/auto.ts
+++ other/discord/src/modules/Other/auto.ts
@@ -7,8 +7,6 @@ import { discordEventBroker } from "../..";
 discordEventBroker.on(Events.GuildMemberAdd, async (member) => {
     const guildId = member.guild.id;
     const userId = member.user.id;
-
-    // ここでデータベースにアクセスして、必要な処理を行う
     // 例: await database.set(guildId, userId, { joinedAt: new Date() });
 
     console.log(`User ${userId} joined guild ${guildId}`);
